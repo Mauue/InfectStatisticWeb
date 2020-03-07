7c13f280a7583d4163f8813a9e8d289bd2d495c8
Mauue <364791516@qq.com>
2020-03-07 23:05:16 +0800

    继续完善项目结构

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from db import init_db_command
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+
+    init_app(app)
+
+    @app.route('/')
+    def hello_world():
+        return 'Hello World!'
+
+    return app
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def init_app(app):
+    app.cli.add_command(init_db_command)
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
